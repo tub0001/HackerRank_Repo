b1c77196a04e08ad902d6508fb7b173ff894ab6e
Vedant Dixit <vedant.d@techaspect.com>
2018-09-30 16:30:15 +0530

    Added challenge Insert a Node at tail in Problem Solving

---
+++ Day_5/insertanodeattail.js
@@ -0,0 +1,19 @@
+function insertNodeAtTail(head, data) {
+    
+    const node = new SinglyLinkedListNode(data);
+            if(head==null){
+                
+            head=node;
+                return head;
+        }
+        else{
+             var current = head;
+            while(current.next!=null){
+                current = current.next;
+            }
+            current.next=node;
+            return head;
+        }
+
+
+}
