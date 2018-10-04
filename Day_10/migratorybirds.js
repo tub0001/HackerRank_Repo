8b11ba4e56e29b8d245811c21d6afda60f7364fb
Vedant Dixit <vedant.d@techaspect.com>
2018-10-04 20:28:16 +0530

    Added Challenge Migratory Birds to Algorithms

---
+++ Day_10/migratorybirds.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the migratoryBirds function below.
+function migratoryBirds(arr) {
+    var max = 0;
+    var maxElement;
+    for(var specie = 1; specie <= 5; specie++) {
+        var freq = 0;
+            for(var i in arr) {
+             if(arr[i] == specie) {
+                freq++;
+                }
+         }
+        if(freq > max) {
+            max = freq;
+            maxElement = specie;
+        }
+    }
+    return maxElement;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
