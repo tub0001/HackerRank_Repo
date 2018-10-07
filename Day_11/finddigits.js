bd18b3dfbbac4d56d83d311b6d607edbd7b3516c
Vedant Dixit <vedant.d@techaspect.com>
2018-10-08 02:06:39 +0530

    Added Challenge Find Digits to Implementations

---
+++ Day_11/finddigits.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the findDigits function below.
+function findDigits(n) {
+    var temp = n;     
+        var result = 0;
+
+        while(temp > 0) {
+          var reminder = Math.floor(temp%10);
+            temp = temp/10;
+            if(reminder > 0 && n % reminder == 0) {
+                result++;
+            }
+        }
+
+        return result;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        let result = findDigits(n);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
