7c19b24d9ceaf12e8a7d9b558e40e306a129b4e6
Vedant Dixit <vedant.d@techaspect.com>
2018-10-08 02:00:11 +0530

    Added Challenge Drawing Book to Implementations

---
+++ Day_11/drawingbook.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the pageCount function below.
+ */
+function pageCount(n, p) {
+    var page1 = Math.floor(Math.abs((p) / 2));
+     if(n%2==0){
+        n++;
+    }
+    var page2 = Math.floor(Math.abs((p - (n)) / 2));
+    return page1 < page2 ? page1 : page2;
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const p = parseInt(readLine(), 10);
+
+    let result = pageCount(n, p);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
