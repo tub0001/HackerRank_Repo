5b25aa410befd6b78a5d8eab387fb152ca6868d6
Vedant Dixit <vedant.d@techaspect.com>
2018-10-08 02:04:07 +0530

    Added Challenge Viral Advertising to Implementations

---
+++ Day_11/viraladvertising.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the viralAdvertising function below.
+function viralAdvertising(n) {
+     var people = 5;
+        var count = 0;
+        
+        for(var i = 0; i < n; i++){
+            var liked = Math.floor(people / 2);
+            people = liked * 3;
+            count = count + liked;
+        }
+
+        return count;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let result = viralAdvertising(n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
