48e36cc175f484e6ee89212d98f98969d3adc0f1
Vedant Dixit <vedant.d@tadigital.com>
2018-10-12 16:48:03 +0530

    Added challenge Counting Sort 1 in Algorithms

---
+++ Day_15/countingsort1.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingSort function below.
+function countingSort(arr) {
+    let counts = Array(100).fill(0);
+    arr.forEach(item => { ++counts[item]; });
+    return counts;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = countingSort(arr);
+
+    ws.write(result.join(" ") + "\n");
+
+    ws.end();
+}
