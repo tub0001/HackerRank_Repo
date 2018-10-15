3cdd4306a0611e8f6a6cc076a541a58a5776c252
Vedant Dixit <vedant.d@tadigital.com>
2018-10-15 12:14:32 +0530

    Added challenge Sherlock and Array to Algorithms

---
+++ Day_16/sherlockandarray.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the balancedSums function below.
+function balancedSums(arr) {
+    var rightSum = 0;
+    var leftSum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        rightSum += arr[i];
+    }
+    for (var i = 0; i < arr.length; ++i) {
+        rightSum -= arr[i]; // sum is now right sum for index i 
+
+        if (leftSum == rightSum)
+            return "YES";
+
+        leftSum += arr[i];
+    }
+    return "NO";
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine().trim(), 10);
+
+    for (let TItr = 0; TItr < T; TItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+        const result = balancedSums(arr);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
